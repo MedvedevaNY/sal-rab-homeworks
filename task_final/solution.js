f9515ea61abdd738e4a7538edf316445c8ad3685
Medvedeva Nadezhda <evstratova94@yandex.ru>
2021-12-08 15:00:41 +0300

    final

--- task_final/solution.js
+++ task_final/solution.js
@@ -3,22 +3,22 @@
 function sendRequest(name, phone, adress, goods, sum) {
     let data = {client, order: {}, goods: []};
     
-    data.client = name + " " +phone;
+    data.client = name + " " + phone;
 
     data.order.address = "ул. " + address.street + ", дом " + adress.house + ", " + adress.entrance + " подъезд, " + adress.floor + "этаж, квартира " + adress.flat ;
     data.order.sum = sum;
 
-    let countOfGoods = goods.length;
-    let goods = {
-        title,
-        counte,
-    }; 
+    let countOfGoods = goods.length; 
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push(goods[i]);
+        let goods = {
+            title: goods[i].title,
+            count: goods[i].count
+        };
+        data.goods.push(goods);
     }
 
-    let jsonData = JSON.stringify({data: data});
+    let jsonData = JSON.stringify({data});
     console.log(data);
 
     return jsonData;
